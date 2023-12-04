9a095460fb4001afc9384566bab9d769baa7469d
Saul Sayers <saulsayers@gmail.com>
2023-12-04 17:48:31 +0700

    feat(firebase) sementara

--- services/user.js
+++ services/user.js
@@ -1,128 +1,122 @@
-const { MongoClient, ObjectId } = require('mongodb');
+const admin = require('firebase-admin');
 
-const uri = "mongodb+srv://test:saul@cluster0.sbzqnvj.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true });
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const serviceAccount = require('serviceaccount.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
 
 async function insertUser(user) {
   try {
-    await client.connect();
-    const database = client.db("elevarm");
-    const collection = database.collection("user");
     const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-    const result = await collection.insertOne({
+    const docRef = db.collection('users').doc();
+    await docRef.set({
       name: user.name,
-      password: user.hashedPassword,
+      password: hashedPassword,
       username: user.username,
       email: user.email,
       account_type: user.account_type,
-      // additional properties here
     });
-    return result;
-  } finally {
-    await client.close();
+    return docRef.id;
+  } catch (error) {
+    console.error("Error adding document: ", error);
   }
 }
 
 async function findAllUsers() {
   try {
-    await client.connect();
-    const database = client.db("elevarm");
-    const collection = database.collection("user");
-    const result = await collection.find({}).toArray();
-    return result;
-  } finally {
-    await client.close();
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.get();
+    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  } catch (error) {
+    console.error("Error getting documents: ", error);
   }
 }
 
 async function deleteUser(userId) {
   try {
-    await client.connect();
-    const database = client.db("elevarm");
-    const collection = database.collection("user");
-    const _id= new ObjectId(userId)
-    const result = await collection.deleteOne({ _id });
-    return result;
-  } finally {
-    await client.close();
+    const userRef = db.collection('users').doc(userId);
+    await userRef.delete();
+    return { id: userId };
+  } catch (error) {
+    console.error("Error removing document: ", error);
   }
 }
 
 async function updateUser(userId, user) {
   try {
-    await client.connect();
-    const database = client.db("elevarm");
-    const collection = database.collection("user");
-    const result = await collection.updateOne({ _id: ObjectId(userId) }, { $set: user });
-    return result;
-  } finally {
-    await client.close();
+    const userRef = db.collection('users').doc(userId);
+    await userRef.update(user);
+    return { id: userId };
+  } catch (error) {
+    console.error("Error updating document: ", error);
   }
 }
 
-async function findById(userId) {
+async function findUserById(userId) {
   try {
-    await client.connect();
-    const database = client.db("elevarm");
-    const collection = database.collection("user");
-    const result = await collection.findOne({ _id: ObjectId(userId) });
-    return result;
-  } finally {
-    await client.close();
+    const userRef = db.collection('users').doc(userId);
+    const doc = await userRef.get();
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      return { id: doc.id, ...doc.data() };
+    }
+  } catch (error) {
+    console.error("Error getting document: ", error);
   }
 }
 
 async function registerUser(user) {
   try {
-    await client.connect();
-    const database = client.db("elevarm");
-    const collection = database.collection("user");
-    // Hash the password before inserting it into the database
-    const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-    // Create a new user object with the hashed password
-    const newUser = {
+    const docRef = db.collection('users').doc();
+    await docRef.set({
       name: user.name,
+      password: user.password,
       username: user.username,
       email: user.email,
-      password: hashedPassword,
-      account_type: "User",
-      // Add any additional fields as needed
-    };
-    console.log(newUser);
-    const result = await collection.insertOne(newUser);
-    return result;
-  } finally {
-    await client.close();
+      account_type: user.account_type,
+    });
+    return docRef.id;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+}
+async function loginUser(user) {
+  try {
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.where('username', '==', user.username).get();
+    
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return null;
+    }  
+
+    let userData = null;
+
+    for (const doc of snapshot.docs) {
+      const data = doc.data();
+      if (await bcrypt.compare(user.password, data.password)) {
+        userData = { id: doc.id, ...data };
+        break; // Exit the loop since we found a match
+      }
+    }
+
+    return userData;
+  } catch (error) {
+    console.error("Error getting documents: ", error);
   }
 }
 
-async function loginUser(user) {
-  try {
-    console.log(user);
-    await client.connect();
-    const database = client.db("elevarm");
-    const collection = database.collection("user");
-    const result = await collection.findOne({ username: user.username });
-    // Compare the hashed password in the database with the user's input
-    console.log(result)
-    if (result && await bcrypt.compare(user.password, result.password)) {
-      return result;
-    } else {
-      return null;
-    }
-  } finally {
-    await client.close();
-  }
-}
 
 module.exports = {
   insertUser,
   findAllUsers,
   deleteUser,
   updateUser,
-  findById,
+  findUserById,
   loginUser,
   registerUser,
 };
